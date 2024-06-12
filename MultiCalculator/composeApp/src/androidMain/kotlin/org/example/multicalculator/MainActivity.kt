9cc88270d04c5dabb709cbbb0715c5d8a7b7100e
SatishCambrian <SatishCambrian@users.noreply.github.com>
2024-06-11 23:07:25 -0400

    Edit the CalcEqualsButton to take a parameter of Mutable String

--- MultiCalculator/composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
+++ MultiCalculator/composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
@@ -5,8 +5,15 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -48,7 +55,13 @@ fun CalcOperationButton(){
 }
 
 @Composable
-fun CalcEqualsButton(){
+fun CalcEqualsButton(display: MutableState<String>) {
+    Button(
+        onClick = { display.value = "0" },
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(text = "=", fontSize = 20.sp)
+    }
 }
 
 
