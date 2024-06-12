51062a9c375bb83ca3f7d5f1d4020cba839d2632
SatishCambrian <SatishCambrian@users.noreply.github.com>
2024-06-11 23:15:58 -0400

    Within CalcView in the first of the Columns created in the last step create a for loop that will count from 7 to 1 in steps of 3. Within the loop call the CalcRow method with the current loop value, 3 buttons, and the display text

--- MultiCalculator/composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
+++ MultiCalculator/composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
@@ -5,6 +5,8 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,7 +15,10 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,7 +43,30 @@ fun AppAndroidPreview() {
 @Preview
 @Composable
 fun CalcView(){
+    val displayText = remember { mutableStateOf("0")}
 
+    Column(modifier = Modifier.background(Color.LightGray)){
+        Row{
+            CalcDisplay(displayText)
+        }
+        Row{
+            Column{
+                for (i in 7 downTo 1 step 3){
+                    CalcRow(displayText,startNum = i, numButtons =3)
+                }
+                Row{
+                    CalcNumericButton(number = 0,display = displayText)
+                    CalcEqualsButton(displayText)
+                }
+            }
+            Column {
+                CalcOperationButton(operation = "+", display = displayText)
+                CalcOperationButton(operation = "-", display = displayText)
+                CalcOperationButton(operation = "*", display = displayText)
+                CalcOperationButton(operation = "/", display = displayText)
+            }
+        }
+    }
 }
 
 @Composable
