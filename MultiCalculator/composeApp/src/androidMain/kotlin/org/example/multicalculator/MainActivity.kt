cd96be501d60c408d7a72f6a1ea56f6c8bfa9fa7
SatishCambrian <SatishCambrian@users.noreply.github.com>
2024-06-11 23:11:55 -0400

    edited CalcDisplay to take MutableState param

--- MultiCalculator/composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
+++ MultiCalculator/composeApp/src/androidMain/kotlin/org/example/multicalculator/MainActivity.kt
@@ -5,6 +5,8 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -44,7 +46,15 @@ fun CalcRow(){
 }
 
 @Composable
-fun CalcDisplay(){
+fun CalcDisplay(display: MutableState<String>) {
+    Text(
+        text = display.value,
+        fontSize = 30.sp,
+        modifier = Modifier
+            .height(50.dp)
+            .padding(5.dp)
+            .fillMaxWidth()
+    )
 }
 
 @Composable
